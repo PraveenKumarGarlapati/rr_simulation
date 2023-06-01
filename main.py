effb79f51ddf760095630ad2255c202645361aa4
Praveen106 Kumar <prvn012@gmail.com>
2023-06-01 23:10:16 +0530

    first_push

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+def main():
+    st.write("This is a test")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,6 @@
+API1 = ""
+
+try:
+    from .private import *
+except Exception:
+    pass
